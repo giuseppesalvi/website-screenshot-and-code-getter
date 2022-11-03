a3781c676ede53f6a00389e9751fe5903583c475
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-11-03 18:22:15 +0100

    refactor: remove old implementations

--- run.py
+++ run.py
@@ -9,44 +9,32 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-#from Screenshot import Screenshot_clipping
+
 
 def get_screenshot(website, domain):
-    print("Generating the screenshot ...")
-    output_option = " --filename='results/" + domain + "'"
-    os.system("pageres " + website + output_option)
-
-def get_screenshot_2(website, domain):
     # start web browser
     options = webdriver.ChromeOptions()
     options.headless = True
     options.add_argument("--window-size=1280,1024")
-    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    # get source code
-    #driver.get(website)
-    #driver.save_screenshot_as_file("results/" + domain + ".png")
-
-    #S = lambda X: driver.execute_script("return document.body.parentNode.scroll" + X)
-    #driver.set_window_size(S("Width"), S("Height"))
-    #driver.find_element(By.TAG_NAME, "body").screenshot("results/" + domain + ".png")
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    #driver.maximize_window()
-    #launch URL
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
+    # launch URL
     driver.get(website)
-    #get window size
+    # get window size
     s = driver.get_window_size()
-    #obtain browser height and width
+    # obtain browser height and width
     w = driver.execute_script('return document.body.parentNode.scrollWidth')
     h = driver.execute_script('return document.body.parentNode.scrollHeight')
-    #set to new window size
+    # set to new window size
     driver.set_window_size(w, h)
-    #obtain screenshot of page within body tag
-    driver.find_element(By.TAG_NAME, "body").screenshot("results/" + domain + ".png")
-    #driver.find_element_by_tag_name('body').screenshot("tutorialspoint.png")
+    # obtain screenshot of page within body tag
+    driver.find_element(By.TAG_NAME, "body").screenshot(
+        "results/" + domain + ".png")
+    # driver.find_element_by_tag_name('body').screenshot("tutorialspoint.png")
     driver.set_window_size(s['width'], s['height'])
     driver.quit()
-    
+
+
 def get_code(website, domain):
     print("\nGenerating the code ...")
 
@@ -56,23 +44,8 @@ def get_code(website, domain):
     with open("results/" + domain + ".html", "w") as f:
         all = dom.find("*")
         for node in all:
-    	    f.write(node.html())
-	
-def get_code_2(website, domain):
-    print("\nGenerating the code ...")
+            f.write(node.html())
 
-    # start web browser
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-    # get source code
-    driver.get(website)
-    html = driver.page_source
-
-    with open("results/" + domain + ".html", "w") as f:
-        f.write(html)
-
-    # close web browser
-    driver.close()
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
@@ -83,7 +56,7 @@ class MyHTMLParser(HTMLParser):
         self.count[tag] += 1
 
     def handle_startendtag(self, tag, attrs):
-        self.count[tag] +=1
+        self.count[tag] += 1
 
 
 def get_log(domain):
@@ -103,7 +76,7 @@ def get_log(domain):
     # Save info in the log file
     with open(filename + ".log", "w") as f:
         print("Number of nodes: ", n_nodes, file=f)
-        print("Number of different elements: ", n_elements , file=f)
+        print("Number of different elements: ", n_elements, file=f)
         print("Divided per element: ", file=f)
         pprint(parser.count, f)
 
@@ -116,7 +89,8 @@ def sort_websites_by_nodes(filepath):
     websites = []
     with open(filepath, "r") as f:
         for line in f:
-            websites.append((line.strip().split(" ")[0], int(line.strip().split(" ")[1])))
+            websites.append(
+                (line.strip().split(" ")[0], int(line.strip().split(" ")[1])))
 
     websites.sort(key=lambda tup: tup[1])
     with open(filepath, "w") as f:
@@ -124,16 +98,18 @@ def sort_websites_by_nodes(filepath):
             f.write(website[0] + " " + str(website[1]) + "\n")
 
 
-
-
 if __name__ == "__main__":
     website_list = []
 
-    parser = argparse.ArgumentParser(description="get screenshot and code for a website", usage="python3 run.py [--website {website_url} | --website_list {file_path}]")
+    parser = argparse.ArgumentParser(description="get screenshot and code for a website",
+                                     usage="python3 run.py [--website {website_url} | --website_list {file_path}]")
     parser.add_argument("--website", help="website url")
-    parser.add_argument("--website_list", help="file path with list of website urls")
-    parser.add_argument("--just_new", action='store_true',  help="process only the websites not already present")
-    parser.add_argument("--task", help="task of the script: get screenshot, get code or both, sort statistics", default="all", choices=["all", "screenshot", "code", "stats"])
+    parser.add_argument(
+        "--website_list", help="file path with list of website urls")
+    parser.add_argument("--just_new", action='store_true',
+                        help="process only the websites not already present")
+    parser.add_argument("--task", help="task of the script: get screenshot, get code or both, sort statistics",
+                        default="all", choices=["all", "screenshot", "code", "stats"])
 
     args = parser.parse_args()
     if args.website:
@@ -145,7 +121,6 @@ if __name__ == "__main__":
     else:
         parser.print_usage()
 
-
     for i, website in enumerate(website_list):
         if website.startswith(" ") or website.startswith("#"):
             continue
@@ -153,19 +128,16 @@ if __name__ == "__main__":
             domain = website.split("//www.")[-1].split("/")[0]
         else:
             domain = website.split("//")[-1].split("/")[0]
-        print("[%d/%d] %s" %(i + 1, len(website_list), domain))
+        print("[%d/%d] %s" % (i + 1, len(website_list), domain))
 
         if args.just_new and isfile("results/" + domain + ".html"):
             print("Already present\n")
             continue
 
         if args.task in ["all", "screenshot"]:
-            get_screenshot_2(website, domain)
+            get_screenshot(website, domain)
         if args.task in ["all", "code"]:
-            get_code_2(website, domain)
+            get_code(website, domain)
             get_log(domain)
         if args.task in ["all", "stats"]:
             sort_websites_by_nodes("results/nodes.log")
-        
-       
-
