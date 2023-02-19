98676ce5c150ffd9c3aec56304a5aa971ff967f4
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2023-02-19 17:26:34 +0100

    feat: summary of stats

--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ from css_parser import parse_css
 import json
 from genericpath import isfile
 import os
+from statistics import mean
 
 def get_statistics(domain, test_name):
     """ Save statistics for the website"""
@@ -99,7 +100,7 @@ def print_stats(website_dict):
     img = Image.open(website_dict["filename"]+ ".png")
     height = img.height
     width = img.width
-    website_dict["screenshot_dimensions "] = [width, height] 
+    website_dict["screenshot_dimensions"] = [width, height] 
 
 
     # add to the dictionary the size of all the lists
@@ -140,10 +141,126 @@ def sum_of_dict_sizes(lst):
 
 
 def stats_summary():
+    summary = {}
+
+    # Sizes
+    summary["css_classes"] = []
+    summary["css_classes_skipped"] = []
+    summary["css_properties"] = []
+    summary["css_properties_skipped"] = []
+    summary["css_urls"] = []
+    summary["css_urls_before_sanitizing"] = []
+    summary["html_classes"] = []
+    summary["html_classes_before_sanitizing"] = []
+    summary["html_tags"] = []
+    summary["html_tags_before_sanitizing"] = []
+
+    # Number of html nodes
+    summary["n_html_nodes"] = [] 
+    summary["n_html_nodes_before_sanitizing"] = []
+
+    # Number of lines
+    summary["n_html_lines"] = []
+    summary["n_html_sanitized_lines"] = []
+    summary["n_css_lines"] = [] 
+
+    # Screenshot photo dimensions
+    summary["screenshot_width"] = []
+    summary["screenshot_height"] = []
+
     for filename in os.listdir("results/"):
         if filename.endswith(".json"):
             with open("results/" + filename) as f:
                 content = json.load(f)
-                #json.
 
+                # Sizes
+                summary["css_classes"].append(content["sizes"]["css_classes"])
+                summary["css_classes_skipped"].append(content["sizes"]["css_classes_skipped"])
+                summary["css_properties"].append(content["sizes"]["css_properties"])
+                summary["css_properties_skipped"].append(content["sizes"]["css_properties_skipped"])
+                summary["css_urls"].append(content["sizes"]["css_urls"])
+                summary["css_urls_before_sanitizing"].append(content["sizes"]["css_urls_before_sanitizing"])
+                summary["html_classes"].append(content["sizes"]["html_classes"])
+                summary["html_classes_before_sanitizing"].append(content["sizes"]["html_classes_before_sanitizing"])
+                summary["html_tags"].append(content["sizes"]["html_tags"])
+                summary["html_tags_before_sanitizing"].append(content["sizes"]["html_tags_before_sanitizing"])
+
+                # Number of html nodes
+                summary["n_html_nodes"].append(content["n_html_nodes"])
+                summary["n_html_nodes_before_sanitizing"].append(content["n_html_nodes_before_sanitizing"])
+
+                # Number of lines
+                n_css_lines = 0
+                for key, value in content["number_of_lines"].items():
+                    if key.endswith("sanitize.html"):
+                        summary["n_html_sanitized_lines"].append(value)
+                    elif key.endswith(".html"):
+                        summary["n_html_lines"].append(value)
+                    elif key.endswith(".css"):
+                        n_css_lines += value
+                
+                # Possibly we have more than one css file
+                summary["n_css_lines"].append(n_css_lines)
+
+                # Screenshot photo dimensions
+                summary["screenshot_width"].append(content["screenshot_dimensions"][0])
+                summary["screenshot_height"].append(content["screenshot_dimensions"][1])
+
+    # Sort Lists
+    # Sizes
+    summary["css_classes"].sort()
+    summary["css_classes_skipped"].sort()
+    summary["css_properties"].sort()
+    summary["css_properties_skipped"].sort()
+    summary["css_urls"].sort()
+    summary["css_urls_before_sanitizing"].sort()
+    summary["html_classes"].sort()
+    summary["html_classes_before_sanitizing"].sort()
+    summary["html_tags"].sort()
+    summary["html_tags_before_sanitizing"].sort()
+
+    # Number of html nodes
+    summary["n_html_nodes"].sort()
+    summary["n_html_nodes_before_sanitizing"].sort()
+
+    # Number of lines
+    summary["n_html_lines"].sort()
+    summary["n_html_sanitized_lines"].sort()
+    summary["n_css_lines"].sort()
+
+    # Screenshot photo dimensions
+    summary["screenshot_width"].sort()
+    summary["screenshot_height"].sort()
+
+
+    # Sizes
+    # Do averages
+    summary["avg_css_classes"] = mean(summary["css_classes"])
+    summary["avg_css_classes_skipped"] = mean(summary["css_classes_skipped"])
+    summary["avg_css_properties"] = mean(summary["css_properties"])
+    summary["avg_css_properties_skipped"] = mean(summary["css_properties_skipped"])
+    summary["avg_css_urls"] = mean(summary["css_urls"])
+    summary["avg_css_urls_before_sanitizing"] = mean(summary["css_urls_before_sanitizing"])
+    summary["avg_html_classes"] = mean(summary["html_classes"])
+    summary["avg_html_classes_before_sanitizing"] = mean(summary["html_classes_before_sanitizing"])
+    summary["avg_html_tags"] = mean(summary["html_tags"])
+    summary["avg_html_tags_before_sanitizing"] = mean(summary["html_tags_before_sanitizing"])
+
+    # Number of html nodes
+    summary["avg_n_html_nodes"] = mean(summary["n_html_nodes"])
+    summary["avg_n_html_nodes_before_sanitizing"] = mean(summary["n_html_nodes_before_sanitizing"])
+
+    # Number of lines
+    summary["avg_n_html_lines"] = mean(summary["n_html_lines"])
+    summary["avg_n_html_sanitized_lines"] = mean(summary["n_html_sanitized_lines"])
+    summary["avg_n_css_lines"] = mean(summary["n_css_lines"])
+
+    summary["n_websites"] = len(summary["n_html_nodes"])
+
+    # Write summary in json file
+    with open("summary.json", "w") as f:
+        json.dump(summary, f, sort_keys=True, indent=2)
     return
+
+if __name__ == "__main__":
+    stats_summary()
